a484b7b2483508dec2a5aad178bf2af178408bc6
ArthurCoelho15 <Arthursantos1518@gmail.com>
2025-02-02 15:28:49 -0300

    Atualização

--- testezasso/Exercicios/Seçao_4/Q3_S4.py
+++ testezasso/Exercicios/Seçao_4/Q3_S4.py
@@ -1,6 +1,6 @@
 """
 
-Faça um programa que recebe um número inteiro e informe se este número é par ou ímpar
+Faça um programa que recebe um número inteiro e informe se este número é par ou ímpar.
 
 """
 
